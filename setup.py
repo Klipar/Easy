30a070bec705e9a0e1bb1111671c4abc07d2ce1e
klipar <bhdnkarpenko123@gmail.com>
2025-07-19 18:51:22 +0300

    updated version of lib

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='easy',
-    version='1.0',
+    version='2.0',
     packages=find_packages(),
     description='A simple library with useful functions to ease your work.',
     author='Klipar',
